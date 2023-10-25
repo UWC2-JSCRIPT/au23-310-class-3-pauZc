5c367843951f05862cb47e13c6ff3129d38e0941
Laura Zanella <zclaurap@gmail.com>
2023-10-24 21:02:02 -0700

    class progress

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -2,8 +2,20 @@
 // with these properties: {descr, price}
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
+const logReceipt = (... items) =>{
+  let total = 0;
+  items.forEach((item)=>{
+    total += item.price;
+    console.log(item.descr)
+  })
+  console.log(total)
+}
+const obj1 = { descr: 'Burguer', price: 11.50 }
+const obj2 = { descr: 'Fish', price: 5.00 }
+const obj3 = { descr: 'French Fries', price: 9.70 }
+const obj4 = { descr: 'Coffe', price: 7.00 }
 
-
+logReceipt(obj1, obj2, obj3, obj4)
 
 // Check
 logReceipt(
@@ -11,6 +23,8 @@ logReceipt(
   { descr: 'Chips & Salsa', price: 2.99 },
   { descr: 'Sprite', price: 1.99 }
 );
+
+
 // should log something like:
 // Burrito - $5.99
 // Chips & Salsa - $2.99
--- spaceShip.js
+++ spaceShip.js
@@ -2,8 +2,22 @@
 // - should set two properties: name and topSpeed
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
-
-
+class SpaceShip{
+    constructor(name, topSpeed){
+        this.name = name
+        this.topSpeed = topSpeed
+    }
+    accelerate(){
+        const { name, topSpeed} =this
+        console.log(`${name} is taking off at ${topSpeed} ft/s... Woohoo!`)
+    }
+}
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+const buzz = new SpaceShip('Buzz', 400);
+const lightyear = new SpaceShip('Lightyear', 1000);
+const saturn = new SpaceShip('Saturn', 1200);
+buzz.accelerate();
+lightyear.accelerate();
+saturn.accelerate();
